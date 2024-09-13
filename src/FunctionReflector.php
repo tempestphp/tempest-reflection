a93c9990ca9e63f08ecce8b04e760996b92a3c36
aidan-casey <aidan@caseyhouse.net>
2024-09-13 22:51:59 +0000

    Adds reflector dependency to console.

---
+++ src/FunctionReflector.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Reflection;
+
+use Closure;
+use Generator;
+use ReflectionFunction as PHPReflectionFunction;
+
+final readonly class FunctionReflector implements Reflector
+{
+    private PHPReflectionFunction $reflectionFunction;
+
+    public function __construct(
+        PHPReflectionFunction|Closure $function
+    ) {
+        $this->reflectionFunction = $function instanceof Closure
+            ? new PHPReflectionFunction($function)
+            : $function;
+    }
+
+    /** @return Generator|\Tempest\Reflection\ParameterReflector[] */
+    public function getParameters(): Generator
+    {
+        foreach ($this->reflectionFunction->getParameters() as $parameter) {
+            yield new ParameterReflector($parameter);
+        }
+    }
+
+    public function getName(): string
+    {
+        return $this->reflectionFunction->getName();
+    }
+
+    public function getShortName(): string
+    {
+        return $this->reflectionFunction->getShortName();
+    }
+
+    public function getFileName(): string
+    {
+        return $this->reflectionFunction->getFileName();
+    }
+
+    public function getStartLine(): int
+    {
+        return (int) $this->reflectionFunction->getStartLine();
+    }
+}
